75c6ec7e43881859090844b022736dada9e53b17
Alejandro Klever Clemente <alejandroklever4197@gmail.com>
2020-04-07 23:58:16 -0400

    fixed up a minimal login bug

--- LaPesetaWebSite/views.py
+++ LaPesetaWebSite/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AnonymousUser
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     if not isinstance(request.user, AnonymousUser):
+        if not hasattr(request.user, 'tienda'):
+            return redirect('tienda:mostrar-tienda')
         return render(request, 'index.html', context={'tienda_id': request.user.tienda.id})
     return render(request, 'index.html')
