9766429e2f583b85bac7bdde1643275f5e058565
Alejandro Klever Clemente <alejandroklever4197@gmail.com>
2020-05-03 14:35:34 -0400

    proyect updated to django 3

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserMetaData(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    foto_de_perfil = models.ImageField()
-    cuenta_bancaria = models.IntegerField()
-
-    def __str__(self):
-        return str(self.user.username)
-
-
-class IUser(User):
-    username = None
-    first_name = None
-    profile_image = None
-    last_name = None
-    email = None
-    password = None
-    user_permissions = None
-    is_superuser = None
+# class UserMetaData(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     foto_de_perfil = models.ImageField()
+#     cuenta_bancaria = models.IntegerField()
+#
+#     def __str__(self):
+#         return str(self.user.username)
+#
+#
+# class IUser(User):
+#     username = None
+#     first_name = None
+#     profile_image = None
+#     last_name = None
+#     email = None
+#     password = None
+#     user_permissions = None
+#     is_superuser = None
